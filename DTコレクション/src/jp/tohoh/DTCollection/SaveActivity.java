b7abcdedc7254d4ae0e5f815dad4a92d6c1446c5
anddev68 <chk392013@gmail.com>
2014-03-02 00:04:50 +0900

    SaveActivity追加

---
+++ DTコレクション/src/jp/tohoh/DTCollection/SaveActivity.java
@@ -0,0 +1,30 @@
+package jp.tohoh.DTCollection;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+
+/*************************************
+ * セーブ画面のアクティビティー
+ *
+ *************************************/
+public class SaveActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(new MainView(this));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		Intent intent = new Intent(this,PrefActivity.class);
+		startActivity(intent);
+		
+		//	getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
